6b0bff701e571e2bd156ab7afbcc9906e856ae19
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-26 19:33:38 +0200

    creating Redux

--- src/MaximApp/components/WeatherProvider.js
+++ src/MaximApp/components/WeatherProvider.js
@@ -1,28 +1,12 @@
 import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { createStore } from 'redux';
-import {
-  rootWeatherReducer,
-  weatherReducer
-} from 'MaximApp/reducers/weatherReducer';
-// import { WeatherContext } from 'MaximApp/context/Context';
+import { weatherReducer } from 'MaximApp/reducers/weatherReducer';
 import { Provider } from 'react-redux';
-const initData = []; //Redux State
-const store = createStore(weatherReducer);
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(weatherReducer, composeWithDevTools());
 const WeatherProvider = ({ children }) => {
-  const [weatherState, weatherDispatch] = useReducer(weatherReducer, initData); //Redux Dispatch
-
-  return (
-    <Provider
-      store={store}
-      // value={{
-      //   weatherState,
-      //   weatherDispatch
-      // }}
-    >
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 //children validation
 WeatherProvider.propTypes = {
