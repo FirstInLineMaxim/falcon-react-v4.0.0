ed1927355ed668005459098566328893ce8f93b0
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-26 20:31:05 +0200

    adding events

--- src/MaximApp/reducers/calendarReducer.js
+++ src/MaximApp/reducers/calendarReducer.js
@@ -1,10 +1,20 @@
-import { ADD_EVENT } from 'MaximApp/redux_types/calendarTypes';
+import {
+  ADD_EVENT,
+  EDIT_EVENT,
+  REMOVE_EVENT
+} from 'MaximApp/redux_types/calendarTypes';
 const initData = [];
 export const calendarReducer = (state = initData, action) => {
   const { type, payload } = action;
   switch (type) {
     case ADD_EVENT: {
       return payload;
+    }
+    case EDIT_EVENT: {
+      return;
+    }
+    case REMOVE_EVENT: {
+      return;
     }
 
     default:
