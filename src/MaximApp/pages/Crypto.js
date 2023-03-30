1cbf7079ea222135dc4ae9a14fc4371dd00c1e76
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-30 15:26:48 +0200

    axios

--- src/MaximApp/pages/Crypto.js
+++ src/MaximApp/pages/Crypto.js
@@ -1,11 +1,13 @@
+import axios from 'axios';
 import TotalOrder from 'MaximApp/components/TotalOrder';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
 import placeholder from '../data/crypto.json';
 export default function Crypto() {
-  const { data } = placeholder;
-  const displayData = data.map(entry => {
+  //   const { data } = placeholder;
+  const [data, setData] = useState();
+  const displayData = data?.map(entry => {
     const {
       quote: { USD }
     } = entry;
@@ -36,14 +38,34 @@ export default function Crypto() {
     const price = [...calculatedNumbers, USD.price];
     return { name: entry.name, price, percent_change_1h };
   });
-  //   function getData() {
-  //     fetch();
-  //   }
+
+  async function getData() {
+    const url =
+      'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&sort=market_cap&cryptocurrency_type=all&tag=all';
+    try {
+      const data = await axios.get(url, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'X-CMC_PRO_API_KEY': `${process.env.REACT_APP_COIN_MARKET_API_KEY}`
+        }
+      });
+      const json = await data.json();
+
+      setData(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <Container fluid>
         <Row className="g-3">
-          {displayData.map((ele, i) => (
+          {displayData?.map((ele, i) => (
             <Col key={i} md={6} lg={6} xxl={4}>
               <TotalOrder key={i} data={ele} />
             </Col>
