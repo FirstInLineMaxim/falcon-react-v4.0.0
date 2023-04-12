8d6c8d9cf148beaed8a4f09813d14f8af9fb5c9d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-04-12 17:52:57 +0200

    Creating Lazy Loading

--- src/MaximApp/WeatherApp.js
+++ src/MaximApp/WeatherApp.js
@@ -1,45 +1,41 @@
 import Weather from 'components/dashboards/default/Weather';
-import React, { useRef, useState } from 'react';
-import { Button, Col, Container, InputGroup, Row } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap/esm';
+import weatherIcon from 'assets/img/icons/weather-icon.png';
+import React, { useEffect, useState } from 'react';
+import { Badge, Col, Container, Form, Row, Spinner } from 'react-bootstrap';
 import { ADD_CITY, REMOVE_CITY } from './redux_types/weatherTypes';
 import { toast } from 'react-toastify';
 import { Dropdown } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import Fuse from 'fuse.js';
-export default function WeatherApp() {
-  const cityInput = useRef();
+import { useDebounce } from 'usehooks-ts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Flex from 'components/common/Flex';
+import FalconCloseButton from 'components/common/FalconCloseButton';
+import { isIterableArray } from 'helpers/utils';
 
+export default function WeatherApp() {
   const weatherApiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
-  // const { weatherState, weatherDispatch } = useContext(WeatherContext);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toggle = () => setDropdownOpen(!dropdownOpen);
   const weatherState = useSelector(state => state);
   const weatherDispatch = useDispatch();
-  //Fetch to get city Name,lat,lon
-
-  async function getCord(city) {
-    try {
-      const data = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${weatherApiKey}`
-      );
-      const [json] = await data.json();
-      const props = {
-        city: json.name,
-        lat: json.lat,
-        lon: json.lon
-      };
-      return props;
-    } catch (error) {
-      console.error(error);
-    }
-  }
   //Creating Lookup for Various Cities
-  const [searchInputValue, setSearchInputValue] = useState('Berlin');
+  const [searchInputValue, setSearchInputValue] = useState(null);
   const [autoCompleteItem, setAutoCompleteItem] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
   //This React hook helps to limit that the component is re-rendered too many times.
-  const debouncedValue = useAsyncDebounce(searchInputValue, 500);
+  const debouncedValue = useDebounce(searchInputValue, 500);
+
+  /**
+   *
+   * @param {string} input
+   *
+   */
   async function awaitCity(input) {
-    const lookup = `https://openweathermap.org/data/2.5/find?&appid=d3085dc00e744cef703a6ec04657f8d8https://openweathermap.org/data/2.5/find?callback=jQuery19101502834364540495_1681306893651&q=${input}&type=like&sort=population&cnt=30&appid=439d4b804bc8187953eb36d2a8c26a02&_=1681306893653`;
+    setLoading(true);
+    const lookup = `https://openweathermap.org/data/2.5/find?&q=${input}&type=like&sort=population&cnt=30&appid=439d4b804bc8187953eb36d2a8c26a02&_=1681306893654`;
     try {
       const json = await fetch(lookup, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -54,30 +50,65 @@ export default function WeatherApp() {
         referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       });
       const data = await json.json();
-      console.log(data);
 
       setAutoCompleteItem(data.list);
+      setLoading(false);
+      return;
     } catch (error) {
       console.error(error);
     }
   }
-  const fuseJsOptions = {
-    keys: ['name']
-  };
-  let searchResult = new Fuse(autoCompleteItem, fuseJsOptions)
-    .search(searchInputValue)
-    .map(item => item.item);
-  console.log(searchResult);
 
-  //
-
+  /**
+   *
+   * @param {object} city
+   * @returns {getWeather}
+   * {@link getWeather}
+   */
+  async function submitNewCity(city) {
+    if (city === '') {
+      return toast(
+        <span className="text-info text-center">
+          Please enter a city First!
+        </span>
+      );
+    }
+    try {
+      const newWeather = await getWeather(city);
+      //Handels duplicates in the state
+      if (
+        weatherState.find(
+          ele =>
+            ele.city === newWeather.city &&
+            ele.coords.lon === newWeather.coords.lon
+        )
+      ) {
+        return toast(
+          <span className="text-info text-center">
+            Cant add same city Twice.
+          </span>
+        );
+      }
+      weatherDispatch({ type: ADD_CITY, payload: newWeather });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  /**
+   *
+   * @param {object}
+   * @returns
+   */
   async function getWeather({ lat, lon, city }) {
     try {
       const data = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`
       );
       const json = await data.json();
+      console.log(json);
+
       const newWeather = {
+        coords: { lat, lon },
         city: city,
         condition: json.weather[0].main,
         precipitation: json.main.humidity,
@@ -91,75 +122,137 @@ export default function WeatherApp() {
       console.error(error);
     }
   }
-
   function handleSubmit(e) {
     if (e.key === 'Enter') {
-      submitNewCity(cityInput.current.value);
+      const item = searchResult[0];
+      e.preventDefault();
+      submitNewCity({
+        lat: item.coord.lat,
+        lon: item.coord.lon,
+        city: item.name
+      });
     }
     return;
-  }
-  async function submitNewCity(city) {
-    if (
-      weatherState.filter(e => e.city.toLowerCase() === city.toLowerCase())
-        .length > 0
-    ) {
-      return toast(
-        <span className="text-info text-center">Cant add same city Twice.</span>
-      );
-    }
-    if (city === '') {
-      return toast(
-        <span className="text-info text-center">
-          Please enter a city First!
-        </span>
-      );
-    }
-    try {
-      const data = await getCord(city);
-      if (!data) {
-        return toast(
-          <span className="text-warning">
-            Seems like this city doesnt Exsist!
-          </span>
-        );
-      }
-      const newWeather = data && (await getWeather(data));
-      //Handels duplicates in the state
-      if (weatherState.find(ele => ele.city === newWeather.city)) {
-        return toast(
-          <span className="text-info text-center">
-            Cant add same city Twice.
-          </span>
-        );
-      }
-      weatherDispatch({ type: ADD_CITY, payload: newWeather });
-    } catch (error) {
-      console.log(error);
-    }
   }
   function removeCity(city) {
     weatherDispatch({ type: REMOVE_CITY, payload: city });
   }
+  //Effect witch only gets run after an Timeout. Awaits user end of Typing
+  useEffect(() => {
+    if (searchInputValue) awaitCity(searchInputValue);
+  }, [debouncedValue]);
+
+  //Effect to set the Search Result
+  useEffect(() => {
+    if (searchInputValue) {
+      const fuseJsOptions = {
+        keys: ['name']
+      };
+      const search = new Fuse(autoCompleteItem, fuseJsOptions)
+        .search(searchInputValue)
+        .map(item => item.item);
+      setSearchResult(search);
+    }
+  }, [autoCompleteItem]);
   return (
     <>
-      <InputGroup className="my-3">
-        <FormControl
-          ref={cityInput}
-          placeholder="City"
-          aria-label="City"
-          aria-describedby="basic-addon2"
-          onChange={e => setSearchInputValue(e.target.value)}
-          onKeyDown={e => handleSubmit(e)}
-        />
-        <Button
-          onClick={() => submitNewCity(cityInput.current.value)}
-          type="submit"
-          variant="info"
-          id="button-addon2"
+      <Dropdown onToggle={toggle} className="search-box mb-4">
+        <Dropdown.Toggle
+          as="div"
+          data-toggle="dropdown"
+          aria-expanded={dropdownOpen}
+          bsPrefix="toggle"
         >
-          ADD
-        </Button>
-      </InputGroup>
+          <Form className="position-relative">
+            <Form.Control
+              type="search"
+              placeholder="Search..."
+              aria-label="Search"
+              className="rounded-pill search-input"
+              value={searchInputValue}
+              onChange={({ target }) => setSearchInputValue(target.value)}
+              onClick={() => setDropdownOpen(false)}
+              onKeyDown={e => handleSubmit(e)}
+            />
+            <FontAwesomeIcon
+              icon="search"
+              className="position-absolute text-400 search-box-icon"
+            />
+
+            {searchInputValue && (
+              <>
+                <div className="search-box-close-btn-container me-4">
+                  {loading && <Spinner size="sm" />}
+                </div>
+                <div
+                  className="search-box-close-btn-container"
+                  // style={{ right: '10px', top: '8px' }}
+                >
+                  <FalconCloseButton
+                    size="sm"
+                    noOutline
+                    onClick={() => setSearchInputValue('')}
+                  />
+                </div>
+              </>
+            )}
+          </Form>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <div className="scrollbar py-3" style={{ maxHeight: '24rem' }}>
+            {isIterableArray(searchResult) && (
+              <>
+                <Dropdown.Header as="h6" className="px-x1 pt-0 pb-2 fw-medium">
+                  Cities
+                </Dropdown.Header>
+                {searchResult.map(item => (
+                  <Dropdown.Item
+                    onClick={() =>
+                      submitNewCity({
+                        lat: item.coord.lat,
+                        lon: item.coord.lon,
+                        city: item.name
+                      })
+                    }
+                    className="fs--1 px-x1 py-1 hover-primary "
+                    key={item.id}
+                  >
+                    <Flex alignItems="center " justifyContent={'between'}>
+                      <span>
+                        <img
+                          className=""
+                          src={
+                            item.weather[0]
+                              ? `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`
+                              : weatherIcon
+                          }
+                          alt=""
+                          height="40"
+                        />
+                        <span>
+                          {item.name},{item.sys.country}
+                        </span>
+                      </span>
+                      <Badge pill bg="200" className={' fs--2'}>
+                        <span className="text-black">
+                          {Number.parseFloat(item.main.temp - 273.15).toFixed(
+                            1
+                          )}
+                          °
+                        </span>
+                      </Badge>
+                    </Flex>
+                  </Dropdown.Item>
+                ))}
+              </>
+            )}
+
+            {searchResult.length === 0 && (
+              <p className="fs-1 fw-bold text-center mb-0">No Result Found.</p>
+            )}
+          </div>
+        </Dropdown.Menu>
+      </Dropdown>
       <Container fluid>
         <Row className="g-3">
           {weatherState.map((ele, i) => (
--- src/MaximApp/layouts/Layout.js
+++ src/MaximApp/layouts/Layout.js
@@ -2,33 +2,38 @@ import CreateEvent from 'components/app/events/create-an-event/CreateEvent';
 import EventDetail from 'components/app/events/event-detail/EventDetail';
 import Error404 from 'components/errors/Error404';
 import Error500 from 'components/errors/Error500';
-import Starter from 'components/pages/Starter';
+// import Starter from 'components/pages/Starter';
 import ErrorLayout from 'layouts/ErrorLayout';
-import AppDashboard from 'MaximApp/pages/AppDashboard';
-import Crypto from 'MaximApp/pages/Crypto';
-import React from 'react';
+// import AppDashboard from 'MaximApp/pages/AppDashboard';
+// import Crypto from 'MaximApp/pages/Crypto';
+import React, { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AppMainLayout from './MainLayout';
+import { Spinner } from 'react-bootstrap';
 
+const Crypto = lazy(() => import('MaximApp/pages/Crypto'));
+const Starter = lazy(() => import('components/pages/Starter'));
+const Calendar = lazy(() => import('MaximApp/pages/AppDashboard'));
 export default function MaximLayout() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<AppMainLayout />}>
-          <Route path="weather" element={<Starter />} />
-          <Route path="dashboard" element={<AppDashboard />} />
-          <Route path="events/create-an-event" element={<CreateEvent />} />
-          <Route path="events/event-detail" element={<EventDetail />} />
-          <Route path="crypto" element={<Crypto />} />
-        </Route>
-        <Route element={<ErrorLayout />}>
-          <Route path="errors/404" element={<Error404 />} />
-          <Route path="errors/500" element={<Error500 />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/errors/404" replace />} />
-      </Routes>
-
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<AppMainLayout />}>
+            <Route path="weather" element={<Starter />} />
+            <Route path="dashboard" element={<Calendar />} />
+            <Route path="events/create-an-event" element={<CreateEvent />} />
+            <Route path="events/event-detail" element={<EventDetail />} />
+            <Route path="crypto" element={<Crypto />} />
+          </Route>
+          <Route element={<ErrorLayout />}>
+            <Route path="errors/404" element={<Error404 />} />
+            <Route path="errors/500" element={<Error500 />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/errors/404" replace />} />
+        </Routes>
+      </Suspense>
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
